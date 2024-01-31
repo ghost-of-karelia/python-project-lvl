86541bd04f399e67b952777dc39e4f93f1c187e3
ghost-of-karelia <mitrijpavlov@gmail.com>
2024-01-31 10:28:22 +0300

    Fix Prime correct answer

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,17 +8,17 @@ def play(rounds_left, name):
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
     for _ in range(rounds_left):
-
+        
         number = randint(9, 100)
         print('Question:', number)
-        correct_answer = 'yes'
 
         answer = request_answer()
+        correct_answer = 'yes'
 
         for i in range(2, int(sqrt(number))):
             if number % i == 0:
                 correct_answer = 'no'
-                break
+            break
 
         if not verify_answer(answer, correct_answer, name):
             return None
