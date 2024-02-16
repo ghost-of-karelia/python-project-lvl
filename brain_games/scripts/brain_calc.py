7bed62d389166fd3b3b6025609af8b3b206fa7c6
ghost-of-karelia <mitrijpavlov@gmail.com>
2024-02-16 10:52:40 +0300

    .

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from brain_games.games import progression
+from brain_games.games import calc
 from brain_games import engine
 
 
 def main():
 
     name = engine.welcome_user()
-    engine.play(name, progression.play)
+    engine.play(name, calc.play)
 
 
 if __name__ == '__main__':
